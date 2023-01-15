1ac53c7bd360d7ab67855c97ec37a3cbbcafaed6
khoaguin <dkn.work@protonmail.com>
2023-01-15 10:14:18 +0700

    done HHE.keygen

--- what-is-hhe/src/main.cpp
+++ what-is-hhe/src/main.cpp
@@ -1,8 +1,65 @@
-#include <pasta/pasta.h>
 #include <iostream>
+#include <vector>
+
+#include <seal/seal.h>
+#include <pasta/pasta.h>
+
+#include "sealhelper.h"
+#include "pastahelper.h"
+
+struct Client {
+    // the HE keys
+    seal::PublicKey he_pk;  // HE public key
+    seal::SecretKey he_sk;  // HE secret key
+    seal::RelinKeys he_rk;  // HE relin key
+    seal::GaloisKeys he_gk;  // HE galois key
+    // the symmetric key
+    std::vector<uint64_t> k;  // the secret symmetric keys
+    // client's data
+    std::vector<seal::Ciphertext> c_k;  // the HE encrypted symmetric keys
+    std::vector<int64_t> m {-2, -1, 0, 1, 2};  // the client's secret data
+    std::vector<uint64_t> c_i;  // the symmetric encrypted data
+    seal::Ciphertext c_res;  // the HE encrypted result received from the server
+};
+
+struct Server {
+    std::vector<seal::Ciphertext> c;  // the HE encrypted data of Client's m
+};
+
 
 int main() {
-    pasta::pasta();
-    std::cout << "This is main.cpp" << std::endl;
+
+    std::cout << "---- Client ----" << std::endl;
+    Client client;
+    std::cout << "The client runs HHE.KeyGen to create the keys" << std::endl;
+    std::shared_ptr<seal::SEALContext> context = sealhelper::get_seal_context();
+    sealhelper::print_parameters(*context);
+    seal::KeyGenerator keygen(*context);
+    keygen.create_public_key(client.he_pk);
+    client.he_sk = keygen.secret_key();
+    keygen.create_relin_keys(client.he_rk);
+    seal::BatchEncoder he_benc(*context);
+    seal::Encryptor he_enc(*context, client.he_pk);
+    seal::Evaluator he_eval(*context);
+    seal::Decryptor he_dec(*context, client.he_sk);
+    bool use_bsgs = false;
+    std::vector<int> gk_indices = pastahelper::add_gk_indices(use_bsgs, he_benc);
+    keygen.create_galois_keys(gk_indices, client.he_gk);
+
+        // size_t params_size = get_seal_params_size(config::plain_mod, config::mod_degree, config::seclevel);
+        // KeyGenerator keygen(*context);
+        // User.he_sk = keygen.secret_key();  // HHE Decryption Secret Key
+        // keygen.create_public_key(User.he_pk);  // HHE Encryption Key
+        // keygen.create_relin_keys(User.he_rk);  // HHE RelinKey
+        // BatchEncoder user_he_benc(*context);
+        // Encryptor user_he_enc(*context, User.he_pk);
+        // Evaluator user_he_eval(*context);
+        // vector<int> gk_indices = add_gk_indices(config::use_bsgs, user_he_benc);
+        // keygen.create_galois_keys(gk_indices, User.he_gk);
+
+
+    std::cout << "The encryption algorithm (HHE.Enc)" << std::endl;
+
+
     return 0;
 }
---
+++ what-is-hhe/src/sealhelper.cpp
@@ -0,0 +1,92 @@
+#include "sealhelper.h"
+
+namespace sealhelper {
+/*
+Helper function: get a SEALContext from parameters.
+*/
+std::shared_ptr<seal::SEALContext> get_seal_context(uint64_t plain_mod, uint64_t mod_degree, int seclevel) {
+    if (seclevel != 128) throw std::runtime_error("Security Level not supported");
+    seal::sec_level_type sec = seal::sec_level_type::tc128;
+
+    seal::EncryptionParameters parms(seal::scheme_type::bfv);
+    parms.set_poly_modulus_degree(mod_degree);
+
+    if (mod_degree == 65536) {
+        sec = seal::sec_level_type::none;
+        parms.set_coeff_modulus(
+            {0xffffffffffc0001, 0xfffffffff840001, 0xfffffffff6a0001,
+            0xfffffffff5a0001, 0xfffffffff2a0001, 0xfffffffff240001,
+            0xffffffffefe0001, 0xffffffffeca0001, 0xffffffffe9e0001,
+            0xffffffffe7c0001, 0xffffffffe740001, 0xffffffffe520001,
+            0xffffffffe4c0001, 0xffffffffe440001, 0xffffffffe400001,
+            0xffffffffdda0001, 0xffffffffdd20001, 0xffffffffdbc0001,
+            0xffffffffdb60001, 0xffffffffd8a0001, 0xffffffffd840001,
+            0xffffffffd6e0001, 0xffffffffd680001, 0xffffffffd2a0001,
+            0xffffffffd000001, 0xffffffffcf00001, 0xffffffffcea0001,
+            0xffffffffcdc0001, 0xffffffffcc40001});  // 1740 bits
+    } else {
+        parms.set_coeff_modulus(seal::CoeffModulus::BFVDefault(mod_degree));
+    }
+
+    parms.set_plain_modulus(plain_mod);
+    std::shared_ptr<seal::SEALContext> context = std::make_shared<seal::SEALContext>(parms, true, sec);
+
+    return context;
+}
+
+/*
+Helper function: Prints the parameters in a SEALContext.
+*/
+void print_parameters(const seal::SEALContext &context)
+{
+    auto &context_data = *context.key_context_data();
+
+    /*
+    Which scheme are we using?
+    */
+    std::string scheme_name;
+    switch (context_data.parms().scheme())
+    {
+    case seal::scheme_type::bfv:
+        scheme_name = "BFV";
+        break;
+    case seal::scheme_type::ckks:
+        scheme_name = "CKKS";
+        break;
+    case seal::scheme_type::bgv:
+        scheme_name = "BGV";
+        break;
+    default:
+        throw std::invalid_argument("unsupported scheme");
+    }
+    std::cout << "/" << std::endl;
+    std::cout << "| Encryption parameters :" << std::endl;
+    std::cout << "|   scheme: " << scheme_name << std::endl;
+    std::cout << "|   poly_modulus_degree: " << context_data.parms().poly_modulus_degree() << std::endl;
+
+    /*
+    Print the size of the true (product) coefficient modulus.
+    */
+    std::cout << "|   coeff_modulus size: ";
+    std::cout << context_data.total_coeff_modulus_bit_count() << " (";
+    auto coeff_modulus = context_data.parms().coeff_modulus();
+    std::size_t coeff_modulus_size = coeff_modulus.size();
+    for (std::size_t i = 0; i < coeff_modulus_size - 1; i++)
+    {
+        std::cout << coeff_modulus[i].bit_count() << " + ";
+    }
+    std::cout << coeff_modulus.back().bit_count();
+    std::cout << ") bits" << std::endl;
+
+    /*
+    For the BFV scheme print the plain_modulus parameter.
+    */
+    if (context_data.parms().scheme() == seal::scheme_type::bfv)
+    {
+        std::cout << "|   plain_modulus: " << context_data.parms().plain_modulus().value() << std::endl;
+    }
+
+    std::cout << "\\" << std::endl;
+}
+
+}  // end of sealhelper namespace
